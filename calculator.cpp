fba20c427a86d5e04e36f8f90d5940a787db3150
Davidmr61 <dmr61@alu.ua.es>
2023-11-29 10:25:45 +0100

    Fallo creado

---
+++ calculator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2, resultado;
+    char operador;
+    
+    cout << "Ingrese un operador (+, -, *, /): ";
+    cin >> operador;
+
+    cout << "Ingrese dos números: ";
+    cin >> num1 >> num2;
+
+    switch (operador) {
+        case '+':
+            resultado = num1 - num2;
+            cout << "Resultado: " << resultado << endl;
+            break;
+
+        
+    }
+
+    return 0;
+}
