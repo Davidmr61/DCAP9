f2318c9fed1de01438756f5e76d3874bb11108c6
Davidmr61 <dmr61@alu.ua.es>
2023-11-29 11:44:31 +0100

    hook_configurado

--- calculator.cpp
+++ calculator.cpp
@@ -3,41 +3,41 @@
 using namespace std;
 
 int main() {
-    double num1, num2, resultado;
-    char operador;
-    
-    cout << "Ingrese un operador (+, -, *, /): ";
-    cin >> operador;
+   double num1, num2, resultado;
+   char operador;
+   
+   cout << "Ingrese un operador (+, -, *, /): ";
+   cin >> operador;
 
-    cout << "Ingrese dos números: ";
-    cin >> num1 >> num2;
+   cout << "Ingrese dos números: ";
+   cin >> num1 >> num2;
 
-    switch (operador) {
-        case '+':
-            resultado = num1 + num2;
-            cout << "Resultado: " << resultado << endl;
-            break
+   switch (operador) {
+       case '+':
+           resultado = num1 + num2;
+           cout << "Resultado: " << resultado << endl;
+           break;
 
-        case '-':
-            resultado = num1 - num2;
-            cout << "Resultado: " << resultado << endl;
-            break;
-        case '*':
-            resultado = num1 * num2;
-            cout << "Resultado: " << resultado << endl;
-            break;
-        case '/':
-            if (num2 != 0) {
-                resultado = num1 / num2;
-                cout << "Resultado: " << resultado << endl;
-            } else {
-                cout << "No se puede dividir por cero." << endl;
-            }
-            break;
-        default:
-            cout << "Operador no válido." << endl;
-            break;
-    }
+       case '-':
+           resultado = num1 - num2;
+           cout << "Resultado: " << resultado << endl;
+           break;
+       case '*':
+           resultado = num1 * num2;
+           cout << "Resultado: " << resultado << endl;
+           break;
+       case '/':
+           if (num2 != 0) {
+               resultado = num1 / num2;
+               cout << "Resultado: " << resultado << endl;
+           } else {
+               cout << "No se puede dividir por cero." << endl;
+           }
+           break;
+       default:
+           cout << "Operador no válido." << endl;
+           break;
+   }
 
-    return 0;
-}
+   return 0;
+};
